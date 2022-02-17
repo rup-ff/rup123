cff278e5a143b186ad046f5bbd55e90faa723bf7
rup-ff <86917835+rup-ff@users.noreply.github.com>
2022-02-17 15:58:09 +0530

    Add files via upload

---
+++ rup.js
@@ -0,0 +1,53 @@
+song="";
+song1="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+function preload()
+{
+song=loadSound("music.mp3");
+song1=loadSound("music2.mp3");
+
+}
+
+function setup()
+{
+    canvas=createCanvas(600,600);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video , modelLoaded);
+    poseNet.on(pose , gotPoses);
+}
+function modelLoaded()
+{
+console.log("POSTNET IS INITIALIZED MY DEAR USER ");
+}
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX=" + leftWristX + "leftWristY" + leftWristY);
+
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("leftWristX=" + leftWristX + "leftWristY" + leftWristY);
+    }
+}
+
+function draw()
+{
+    image(video,0,0,600,500);
+}   
+function start()
+{
+    song.play();
+    song1.play();
+    song.setVolume(1);
+    song.rate(1);
+}
